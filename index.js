6cffa121430739bd9bf451210609a962e70c94b0
Fareed Abbasi <fareedabbas072075@gmail.com>
2025-02-07 16:24:29 +0500

    Update index.js wth new unctionalities

--- index.js
+++ index.js
@@ -23,9 +23,33 @@ app.post('/api/enquiry-insert', (req, res) => {
     })
 })
 
+app.get("/api/enquiry-list", async (req, res) => {
+    let enquireyList = await enquireyModel.find();
+    res.send({status: 1, msg: "Enquiry list", data:enquireyList})
+})
+
+app.delete("/api/enquiry-delete/:id", async (req, res) => {
+    let enquireyId = req.params.id;
+    let deletedEnquiry = await enquireyModel.deleteOne({_id:enquireyId})
+    res.send({status: 1, msg: "Enquiry deleted successfully", id: enquireyId, delRes: deletedEnquiry})
+})
+
+app.put("/api/enquiry-update/:id", async (req, res) => {
+    let enquireyId = req.params.id;
+    let {sName, sEmail, sPhone, sMessage} = req.body;
+    let updateObj = {
+        name: sName,
+        email: sEmail,
+        phone: sPhone,
+        message: sMessage
+    };
+    let updateRes = await enquireyModel.updateOne({_id: enquireyId}, updateObj)
+    res.send({status: 1, message: "Enquired updated success", updateRes})
+})
+
 mongoose.connect(process.env.DBURL).then(()=> {
     console.log("Connected to MongoDB")
     app.listen(process.env.PORT,() => {
-        console.log("Server is running on port " + process.env.PORT);
+        console.log("Server is running on port" + process.env.PORT);
     })
 })
